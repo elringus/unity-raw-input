c940647f0b8fcce6c5d4d36117b7a21d1049df4b
Artyom Sovetnikov <elringus@yandex.ru>
2018-04-12 21:51:39 +0300

    initial commit

---
+++ Assets/Scripts/LogRawKeyInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityRawInput;
+
+public class LogRawKeyInput : MonoBehaviour
+{
+    public bool WorkInBackground;
+    public bool InterceptMessages;
+
+    private void OnEnable ()
+    {
+        RawKeyInput.Start(WorkInBackground);
+        RawKeyInput.OnKeyUp += LogKeyUp;
+        RawKeyInput.OnKeyDown += LogKeyDown;
+    }
+
+    private void OnDisable ()
+    {
+        RawKeyInput.Stop();
+        RawKeyInput.OnKeyUp -= LogKeyUp;
+        RawKeyInput.OnKeyDown -= LogKeyDown;
+    }
+
+    private void OnValidate ()
+    {
+        RawKeyInput.InterceptMessages = InterceptMessages;
+    }
+
+    private void LogKeyUp (RawKey key)
+    {
+        Debug.Log("Key Up: " + key);
+    }
+
+    private void LogKeyDown (RawKey key)
+    {
+        Debug.Log("Key Down: " + key);
+    }
+}
